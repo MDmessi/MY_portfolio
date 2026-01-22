7a366e0351b994fdf1fa741f874387ca272b7eaa
MDmessi <sapkotaabiral3@gmail.com>
2026-01-22 15:32:57 +0545

    Added SEO meta tags to index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import styles from  "./App.module.css";
-import { About } from "./components/About/about";
+import { About } from "./components/About/About.jsx";
 import { Contact } from "./components/Contact/Contact";
 import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
